fd7176f7eaa0cf7d3a7e48d1090628da39dc8124
Basit Adeosun <92060452+Bassitt@users.noreply.github.com>
2022-11-26 22:06:57 +0100

    Stage 2/6: Ingredient calculator
    Description
    
    Now let's consider a case where you need a lot of coffee. Maybe you're hosting a party with a lot of guests! In these circumstances, it's better to make preparations in advance.
    
    So, we will ask a user to enter the desired amount of coffee, in cups. Given this, you can adjust the program by calculating how much water, coffee, and milk are necessary to make the specified amount of coffee.
    
    Of course, all this coffee is not needed right now, so at this stage, the coffee machine doesn't actually make any coffee.
    
    Objectives
    
    Let's break the task into several steps:
    
    1. First, read the numbers of coffee drinks from the input.
    2. Figure out how much of each ingredient the machine will need. Note that one cup of coffee made on this coffee machine contains 200 ml of water, 50 ml of milk, and 15 g of coffee beans.
    3. Output the required ingredient amounts back to the user.

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # Coffee-Machine
 
-<b>Example</b>
+<b>Examples</b>
 
-Take a look at the sample output below and print all the following lines.
+The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
 
-Output:
+<b>Example 1:</b> a dialogue with a user might look like this
 
 <pre>
-Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!
+Write how many cups of coffee you will need: 
+> 25
+For 25 cups of coffee you will need:
+5000 ml of water
+1250 ml of milk
+375 g of coffee beans
+</pre>
+
+<b>Example 2:</b> here is another dialogue
+
+<pre>
+Write how many cups of coffee you will need: 
+> 125
+For 125 cups of coffee you will need:
+25000 ml of water
+6250 ml of milk
+1875 g of coffee beans
 </pre>
--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -1,13 +1,19 @@
 package machine;
+import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need: ");
+        int noOfCups = scanner.nextInt();
+
+        final int waterSize = noOfCups * 200;
+        final int milkSize = noOfCups * 50;
+        final int cofeeBeansSize = noOfCups * 15;
+        System.out.println("For " + noOfCups + " cups of coffee you will need:");
+
+        System.out.println(waterSize + " ml of water");
+        System.out.println(milkSize + " ml of milk");
+        System.out.println(cofeeBeansSize + " g of coffee beans");
     }
 }
